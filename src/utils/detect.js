2c0805b4c032378f98acfba0d423b634e679c064
pig_0oo0 <cbj973@gmail.com>
2024-09-03 15:53:33 +0900

    v10의 출력형식에 맞게 수정

--- src/utils/detect.js
+++ src/utils/detect.js
@@ -45,52 +45,56 @@ const preprocess = (source, modelWidth, modelHeight) => {
  * @param {HTMLCanvasElement} canvasRef canvas reference
  * @param {VoidFunction} callback function to run after detection process
  */
+
 export const detect = async (source, model, canvasRef, callback = () => {}) => {
-  const [modelWidth, modelHeight] = model.inputShape.slice(1, 3); // get model width and height
+  const [modelWidth, modelHeight] = model.inputShape.slice(1, 3);
 
-  tf.engine().startScope(); // start scoping tf engine
-  const [input, xRatio, yRatio] = preprocess(source, modelWidth, modelHeight); // preprocess image
+  tf.engine().startScope();
+  const [input, xRatio, yRatio] = preprocess(source, modelWidth, modelHeight);
 
-  const res = model.net.execute(input); // inference model
-  const transRes = res.transpose([0, 2, 1]); // transpose result [b, det, n] => [b, n, det]
-  const boxes = tf.tidy(() => {
-    const w = transRes.slice([0, 0, 2], [-1, -1, 1]); // get width
-    const h = transRes.slice([0, 0, 3], [-1, -1, 1]); // get height
-    const x1 = tf.sub(transRes.slice([0, 0, 0], [-1, -1, 1]), tf.div(w, 2)); // x1
-    const y1 = tf.sub(transRes.slice([0, 0, 1], [-1, -1, 1]), tf.div(h, 2)); // y1
-    return tf
-      .concat(
-        [
-          y1,
-          x1,
-          tf.add(y1, h), //y2
-          tf.add(x1, w), //x2
-        ],
-        2
-      )
-      .squeeze();
-  }); // process boxes [y1, x1, y2, x2]
+  const [boxesTensor, scoresTensor] = model.net.execute(input);
 
-  const [scores, classes] = tf.tidy(() => {
-    // class scores
-    const rawScores = transRes.slice([0, 0, 4], [-1, -1, numClass]).squeeze(0); // #6 only squeeze axis 0 to handle only 1 class models
-    return [rawScores.max(1), rawScores.argMax(1)];
-  }); // get max scores and classes index
+  const [boxes, scores, classes] = tf.tidy(() => {
+    // 바운딩 박스 처리
+    const boxes = boxesTensor.squeeze();
+    const [x1, y1, x2, y2, objectness, classScore] = tf.split(boxes, 6, -1);
+    const processedBoxes = tf.concat([y1, x1, y2, x2], -1);
 
-  const nms = await tf.image.nonMaxSuppressionAsync(boxes, scores, 500, 0.45, 0.2); // NMS to filter boxes
+    // 점수 처리
+    const scores = scoresTensor.squeeze();
 
-  const boxes_data = boxes.gather(nms, 0).dataSync(); // indexing boxes by nms index
-  const scores_data = scores.gather(nms, 0).dataSync(); // indexing scores by nms index
-  const classes_data = classes.gather(nms, 0).dataSync(); // indexing classes by nms index
+    // 클래스 처리 (여기서는 단일 클래스로 가정)
+    const classes = tf.zeros(scores.shape, "int32");
 
-  renderBoxes(canvasRef, boxes_data, scores_data, classes_data, [xRatio, yRatio]); // render boxes
-  tf.dispose([res, transRes, boxes, scores, classes, nms]); // clear memory
+    return [processedBoxes, scores, classes];
+  });
+
+  console.log("Boxes shape:", boxes.shape);
+  console.log("Scores shape:", scores.shape);
+  console.log("Classes shape:", classes.shape);
+
+  const nms = await tf.image.nonMaxSuppressionAsync(
+    boxes,
+    scores,
+    500,
+    0.45,
+    0.2
+  );
+
+  const boxes_data = boxes.gather(nms, 0).dataSync();
+  const scores_data = scores.gather(nms, 0).dataSync();
+  const classes_data = classes.gather(nms, 0).dataSync();
+
+  renderBoxes(canvasRef, boxes_data, scores_data, classes_data, [
+    xRatio,
+    yRatio,
+  ]);
+  tf.dispose([boxesTensor, scoresTensor, boxes, scores, classes, nms]);
 
   callback();
 
-  tf.engine().endScope(); // end of scoping
+  tf.engine().endScope();
 };
-
 /**
  * Function to detect video from every source.
  * @param {HTMLVideoElement} vidSource video source
